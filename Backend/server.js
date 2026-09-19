174f68a725814b50071e0056b30da4f711ec08b9
imsk18 <kumarshatrudhan94263@gmail.com>
2026-09-19 10:42:17 +0530

    add google gimini using laingchain

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,12 @@ dotenv.config();
 
 import app from "./src/app.js"
 import connectToDb from './src/config/database.js'
+import { testAi } from './src/services/ai.service.js';
 
 
 const PORT = process.env.PORT 
 connectToDb()
+testAi();
 
 app.listen(PORT,()=>{
     console.log("server is running",PORT);
---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,21 @@
+import "dotenv/config"
+import {ChatGoogleGenerativeAI} from "@langchain/google-genai";
+
+const model =new ChatGoogleGenerativeAI({
+    model:"gemini-3.8-flash",
+    apiKey:process.env.GIMINI_API_KEY
+});
+
+export async function testAi() {
+ const response =   await model.invoke("write a simple helo world js code")
+    // .then((response)=>{
+    //      console.log(response.content);
+    //      console.log(response.text);
+    // })
+     console.log(response.text);
+   
+}
+
+
+
+
